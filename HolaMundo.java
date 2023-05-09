f148ff035a9f23115b12b147e8ea391fa9b36d8c
Sebastian Diaz Ticona <sdiazt@unsa.edu.pe>
2023-05-09 12:02:12 -0500

    Se Modifico la salida del archivo HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main(String args[]) {
-        System.out.println("Bienvenida al curso");
+        System.out.println("Sebastian Diaz Ticona");
     }
 }
